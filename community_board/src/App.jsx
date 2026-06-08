0c0e83c6e4efa1523ad6026c6ab8efd4ac53043b
Ryan O'Malley <cro5058@g.rit.edu>
2026-06-07 20:33:36 -0400

    App.jsx: Changed title and header text

--- community_board/src/App.jsx
+++ community_board/src/App.jsx
@@ -2,11 +2,10 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Itinerary for 7 days in Chicago</h1>
+      <h1>Rochester, New York Parks</h1>
       <h2>
-        Welcome to Chicago, Natalia! 
-        Check out this calendar to get to know the city 
-        and see all the sights during your stay.
+        Visit the links below to get information about parks
+        in and around Rochester, New York!
       </h2>
     </div>
   )
